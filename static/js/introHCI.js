82dd28ed5a11b04909c7763433e5d20ca9fd7cd5
Albert Lin <albertlin@Dont-Worry-About-It.local>
2016-10-13 19:25:31 -0700

    jquery

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -10,9 +10,29 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$(".jumbotron h1").text("Javascript has taken control");
+		$('#testjs').text("ialusghflawhe");
+		$(".jumbotron p").toggleClass("active");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+	$("a.thumbnail").click(projectClick);
+}
+
+//projectClick
+function projectClick(e) {
+		//	Cancel	the	default	action,	which	prevents	the	page	from	reloading
+		e.preventDefault();
+		//	In	an	event	listener,	$(this)	is	the	element	that	fired	the	event
+		var	projectTitle = $(this).find("p").text();
+		var	jumbotronHeader	= $(".jumbotron h1");
+		jumbotronHeader.text(projectTitle);	
+		var check = 0;
+
+		var	containingProject = $(this).closest(".project");
+		var	description = $(containingProject).find(".project-description");	
+
+		$(description).toggle();
+
 }
